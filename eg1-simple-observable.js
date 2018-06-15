8c60d2dfd0563ec7e9a2e7abaed8452c89a52fd0
Sutee S <unlimited.tee@gmail.com>
2018-06-15 19:21:50 +0700

    add example 1

---
+++ eg1-simple-observable.js
@@ -0,0 +1,14 @@
+const { Observable } = require('rxjs')
+
+const wordStream = Observable.create(observer => {
+  observer.next('Hello,')
+  observer.next('my name is')
+  observer.next('T')
+  observer.next('and')
+  observer.next('I am FOREVER ALONE')
+  observer.next('.')
+
+  observer.complete()
+})
+
+wordStream.subscribe(console.log)
